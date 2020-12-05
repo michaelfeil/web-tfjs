457add64d742db6bda1013414b86a76399d33d7f
michaelfeil <63565275+michaelfeil@users.noreply.github.com>
2020-12-05 21:53:07 +0100

    Updates

---
+++ precache-manifest.d22551351e55cbd3ccea9d47301b8e07.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b183c46bb7545a35b513ee40ba15a463",
+    "url": "/web-tfjs/index.html"
+  },
+  {
+    "revision": "962db4fb9bfc2e967786",
+    "url": "/web-tfjs/static/css/2.190b490a.chunk.css"
+  },
+  {
+    "revision": "bae7f276d2e1a9958093",
+    "url": "/web-tfjs/static/css/main.bb07877f.chunk.css"
+  },
+  {
+    "revision": "962db4fb9bfc2e967786",
+    "url": "/web-tfjs/static/js/2.6bfd1eca.chunk.js"
+  },
+  {
+    "revision": "bae7f276d2e1a9958093",
+    "url": "/web-tfjs/static/js/main.41e252d1.chunk.js"
+  },
+  {
+    "revision": "fd2e1bc0b77c4b3c04f7",
+    "url": "/web-tfjs/static/js/runtime~main.56522a6d.js"
+  }
+]);
